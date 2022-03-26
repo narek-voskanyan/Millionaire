fcc4a8e94a42803ec43b84574311fb8c799b2799
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-26 22:28:38 +0400

    Initial commit

---
+++ src/TVGame/Questions.java
@@ -0,0 +1,20 @@
+package TVGame;
+
+public class Questions {
+    Questions(int status, String question, String trueAnswers, String answerTwo, String answerThree, String answerFour ){
+        this.status = status;
+        this.question = question;
+        this.trueAnswers = trueAnswers;
+        this.answerTwo = answerTwo;
+        this.answerThree = answerThree;
+        this.answerFour = answerFour;
+    }
+
+    private final int status;
+    private final String question;
+    private final String answerTwo;
+    private final String answerThree;
+    private final String trueAnswers;
+    private final String answerFour;
+
+}
