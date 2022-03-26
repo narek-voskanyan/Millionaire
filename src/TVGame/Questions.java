c09af3cf69149214e94290e158fc1547b9755729
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-26 23:11:29 +0400

    how want to be a millionaire

--- src/TVGame/Questions.java
+++ src/TVGame/Questions.java
@@ -1,5 +1,10 @@
 package TVGame;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Questions {
     Questions(int status, String question, String trueAnswers, String answerTwo, String answerThree, String answerFour ){
         this.status = status;
@@ -10,14 +15,27 @@ public class Questions {
         this.answerFour = answerFour;
     }
     public void getQuestion(){
+        ArrayList<String> answers = new ArrayList<>(4);
+        answers.add(trueAnswers);
+        answers.add(answerTwo);
+        answers.add(answerThree);
+        answers.add(answerFour);
+        Collections.shuffle(answers);
+        System.out.println("A" + "( " + answers.get(0) + " )" + "B" + "( " + answers.get(1) + " )" + "\n" +
+                "C" + "( " + answers.get(2) + " )" + "D" + "( " + answers.get(3) + " )");
 
+
+    }
+
+    public int getStatus(){
+        return status;
     }
 
     private final int status;
     private final String question;
+    private final String trueAnswers;
     private final String answerTwo;
     private final String answerThree;
-    private final String trueAnswers;
     private final String answerFour;
 
 }
