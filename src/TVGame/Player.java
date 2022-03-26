1269f379978b76751594f3bc99771ab3618df723
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-27 00:25:58 +0400

    how want to be a millionaire

--- src/TVGame/Player.java
+++ src/TVGame/Player.java
@@ -1,4 +1,15 @@
 package TVGame;
 
 public class Player {
+
+    public void getPlayerInfo(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+    private String name;
+    private int age;
+    private int cash;
 }
